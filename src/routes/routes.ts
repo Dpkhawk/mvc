fb8322cbc64b5861b5768122b2f2b426cd08c2f2
Deepak Kumar <156912379+Dpkhawk@users.noreply.github.com>
2024-05-02 09:01:40 +0530

    Update routes.ts

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -5,7 +5,7 @@ const newProduct = new Product();
 const route: Router = express.Router();
 
 route.get('/users/:id?', newProduct.getAllUsers);
-route.delete('/users/:id', newProduct.deleteUsers); // Uncomment if you have implemented the deleteUsers method
+route.delete('/users/:id', newProduct.deleteUsers); 
 route.post('/users', newProduct.createUser);
 route.put('/users/:id', newProduct.updateUser);
 export default route;
